e3f248ddcfd273b8da8028b2d1d151f5608d0c6f
AnthonMS <Anthon@Steiness.info>
2021-04-26 09:32:02 +0200

    Added The base html code for listing the breweries and I have also added JQuery and Ajax to call the API. So we can now search for Breweries. It will show the first 5. And when clicking load more, it will load the next 5 breweries.

---
+++ public/main.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+    console.log("Main Script Loaded");
+    var page = 1;
+    
+    $("#search-btn").click(function(event){
+        // console.log($("#search").val());
+
+        // https://api.openbrewerydb.org/breweries/search?query=Death%20Avenue&per_page=5&page=1
+        var searchStr = encodeURI($("#search").val());
+        $.ajax({
+            type: "GET",
+            url: `https://api.openbrewerydb.org/breweries/search?query=${searchStr}&per_page=5&page=1`,
+            success: function(data){
+                console.log(data);
+                loadBreweryInUI(data);
+                page = page + 1
+            },
+            fail: function(data) {
+                console.log("FAILED");
+            },
+            always: function(data) {
+                console.log("ALWAYS");
+            }
+        });
+
+    });
+
+    
+    $("#load-more-btn").click(function(event){
+        // console.log("Load More");
+
+        // https://api.openbrewerydb.org/breweries/search?query=Death%20Avenue&per_page=5&page=1
+        var searchStr = encodeURI($("#search").val());
+        $.ajax({
+            type: "GET",
+            url: `https://api.openbrewerydb.org/breweries/search?query=${searchStr}&per_page=5&page=${page}`,
+            success: function(data){
+                // console.log(data);
+                loadBreweryInUI(data);
+                page = page + 1
+            },
+            fail: function(data) {
+                console.log("FAILED");
+            },
+            always: function(data) {
+                console.log("ALWAYS");
+            }
+        });
+
+    });
+
+    
+});
+
+
+function loadBreweryInUI(data) {
+    // console.log(data);
+    for (i = 0; i < data.length; i++) {
+        // console.log(data[i]);
+        // We Need name, brewery_type, state, city, website_url
+        var breweryElement = $(`
+        <li class="list-group-item">
+          <div>
+            <div class="card-body">
+              <h5 class="card-title">${data[i].name}</h5>
+              <h6 class="card-subtitle mb-2">${data[i].brewery_type}</h6>
+              <h6 class="card-subtitle mb-2 text-muted">${data[i].state} - ${data[i].city}</h6><a href="${data[i].website_url}"
+                class="card-link">Link to website</a>
+            </div>
+          </div>
+        </li>
+        `);
+
+        
+        $("#brewery-list").append(breweryElement);
+    }
+}
